db1ba560b31928df68da9a1e88c6435c2db070af
viveks5898 <singh.vivek5898@gmail.com>
2024-01-27 17:05:10 -0800

    isPalindrome question

---
+++ server.js
@@ -0,0 +1,21 @@
+const isPalindrome = (asd) => {
+  let convertArray = asd.split("");
+  let convertArrayReverse = asd.split("").reverse();
+  console.log();
+  for (let i = 0; i < convertArray.length; i++) {
+    for (let j = 0; j < convertArrayReverse.length; j++) {
+      if (convertArray[i] == convertArrayReverse[j]) {
+        return true;
+      }
+      return false;
+    }
+  }
+};
+const isPalindromes = (str) => {
+  const cleanStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+
+  return cleanStr === cleanStr.split("").reverse().join("");
+};
+console.log(isPalindromes("racecar"));
+console.log(isPalindromes("A man, a plan, a canal, Panama!"));
+console.log(isPalindromes("hello"));
