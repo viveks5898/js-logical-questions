23a509286b391746814eb3f857afb39ecd846ae7
viveks5898 <singh.vivek5898@gmail.com>
2024-02-09 01:19:27 +0530

    return even array

---
+++ JS-question/returnEvenArr.js
@@ -0,0 +1,13 @@
+const returnEvenArr = (array) => {
+    let store = [];
+    for (let ele of array) {
+      if (ele % 2 == 0) {
+        store.push(ele);
+      }
+    }
+    return store;
+  };
+  
+  const arr = [1, 41, 2, 3, 9, 4, 5, 6];
+  console.log("first", returnEvenArr(arr));
+  
