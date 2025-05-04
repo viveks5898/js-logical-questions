f91c939971a2a948bc0f364aa7552f41149db7a3
viveks5898 <singh.vivek5898@gmail.com>
2025-05-04 14:44:39 +0530

    first commit

--- JS-question/AddelementetoArray.js
+++
@@ -1,4 +0,0 @@
-const array = ['jan', 'feb', 'april','may']
-
-array.splice(2,0,'march')
-console.log(array)
--- JS-question/reverseThroughString.js
+++
@@ -1,9 +0,0 @@
-// reverse the string using for loop
-const reverseThroughString = (str) => {
-    let data = "";
-    for (let ele of str) {
-      data = ele + data;
-    }
-    return data;
-  };
-  
