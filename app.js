a55b9d240328fc5eba625ca60d63727ea716b31e
viveks5898 <singh.vivek5898@gmail.com>
2025-05-19 09:16:35 +0530

    closure simple question for understandind

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
 
-function createCounter() {
-  let i = 0;
-  return {
-    increment: () => ++i,
-    decrement: () => --i,
-    reset: () => 0,
-  };
-}
-
-const counter = createCounter();
-
-console.log(counter.increment()); // 1
-console.log(counter.increment()); // 2
-console.log(counter.decrement()); // 1
-console.log(counter.reset()); // 0
