5c23cddf1f1152c15da37542c6216189ff08f277
viveks5898 <singh.vivek5898@gmail.com>
2025-07-04 17:31:07 +0530

    added new question

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-function shuffle(array) {
-  // 1. Make a shallow copy so we don’t mutate the original input:
-  const data = array.slice();
 
-  // 2. Starting from the end of the array, move backwards:
-  for (let i = data.length - 1; i > 0; i--) {
-    // 3. Pick a random index j between 0 and i (inclusive):
-    const j = Math.floor(Math.random() * (i + 1));
-    // 4. Swap the elements at positions i and j:
-    [data[i], data[j]] = [data[j], data[i]];
+function removeDuplicates(arr) {
+  const n = arr.length;
+
+  let store = [];
+  let obj = {};
+  for (let i = 0; i < n; i++) {
+    const element = arr[i];
+    if (!obj[element]) {
+      obj[element] = element;
+      store.push(element);
+    }
   }
-
-  // 5. Return the shuffled copy:
-  return data;
+  return store;
 }
-
-
-//For the purpose of user debugging.
-console.log("shuffle", shuffle([1, 2, 3, 4, 5]));
+console.log("res", removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
