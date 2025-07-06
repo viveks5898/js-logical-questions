230403d2efc1455f0a94995ae2a5728075234ba3
viveks5898 <singh.vivek5898@gmail.com>
2025-07-07 03:16:16 +0530

    remove falsy

--- questions/removeFalsy.js
+++ questions/removeFalsy.js
@@ -16,18 +16,15 @@
 // )
 
 
-function removeFalsy (arr){
-const falseValue =[0,  false,  '', null, undefined, NaN]
-const n = arr.length;
-let newArr = []
-for(let i =0; i < n; i++){
-const ele = arr[i]
-if(!falseValue.includes(ele))newArr.push(ele)
-}
-return newArr
+function removeFalsy(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
 
-console.log('res',
-
-  removeFalsy([0, 1, false, 2, '', 3, null, undefined, NaN])
-)
+console.log(removeFalsy([0, 1, false, 2, '', 3, null, undefined, NaN]));
+// Output: [1, 2, 3]
